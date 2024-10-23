005107a5b3a6e9613bbe4ef588599ae6d513417d
ZARRADIHEB <126171975+ZARRADIHEB@users.noreply.github.com>
2024-10-23 12:55:00 +0100

    Update data-entry.js

--- data-entry.js
+++ data-entry.js
@@ -3,7 +3,7 @@ function displayData()
   let name = document.getElementById("name").value;
   let color = document.getElementById("color").value;
 
-  let nameInput = document.getElementById("outputName").innerText=('The you choose is : ' + name)
+  let nameInput = document.getElementById("outputName").innerText=('The Name you choose is : ' + name)
   let colorInput = document.getElementById('outputColor').innerText=('The Favorite Color you choose is : ' + color)
   
   
