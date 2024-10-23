418c196a662697fac0824bf056dbc51d6c1a7f70
ZarradIheb <zarradiheb007@gmail.com>
2024-10-23 12:48:44 +0100

    first commit

---
+++ data-entry.js
@@ -0,0 +1,15 @@
+function displayData()
+{
+  let name = document.getElementById("name").value;
+  let color = document.getElementById("color").value;
+
+  let nameInput = document.getElementById("outputName").innerText=('The you choose is : ' + name)
+  let colorInput = document.getElementById('outputColor').innerText=('The Favorite Color you choose is : ' + color)
+  
+  
+}
+document.addEventListener('keydown', function(event) {
+  if (event.key === "Enter") {
+    displayData();
+  }
+});
